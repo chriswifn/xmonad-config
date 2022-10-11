1892fcaffd8c3a33686f7d6e96cf1ace643b2156
Christian Hageloch <christian.hageloch@gmail.com>
2022-10-11 17:38:23 +0200

    Add tabbed layout instead of tiled (noborders)

--- lib/Custom/MyLayouts.hs
+++ lib/Custom/MyLayouts.hs
@@ -6,6 +6,7 @@ import XMonad
 import XMonad.Layout.NoBorders
 -- toggle Layouts
 import XMonad.Layout.ToggleLayouts (toggleLayouts)
+import XMonad.Layout.Tabbed
 
 import XMonad.Layout.ShowWName
 
@@ -17,6 +18,16 @@ myShowWNameTheme = def
   , swn_color             = "#ffffff"
   }
 
+myTabConfig = def { fontName = "xft:Monoid:regular:size=10:antialias=true:hinting=true"
+                  , activeColor = "#323232"
+                  , inactiveColor = "#1e1e1e"
+                  , activeBorderColor = "#ffffff"
+                  , inactiveBorderColor = "#ccdfe7"
+                  , activeTextColor = "#ffffff"
+                  , inactiveTextColor = "#ccdfe7"
+                  }
+                  
+
 -- smartBorders removes the border if there is only one screen and only
 -- one client on a workspace
 -- noBorders removes the border. In this case I only use this for the
@@ -26,7 +37,7 @@ myLayoutHook = showWName' myShowWNameTheme $ toggleLayouts (noBorders Full) $ sm
 -- I only use tiled and noBorders tiled here because I  toggle
 -- fullscreen layout in MyKeys.hs
 -- All the layouts that I use: tiled (Tall), tiled (Tall) but with no borders, fullscreen (Full)
-myLayout = tiled ||| noBorders tiled
+myLayout = tiled ||| noBorders (tabbed shrinkText myTabConfig)
   where
      -- default tiling algorithm partitions the screen into two panes
      tiled   = Tall nmaster delta ratio
