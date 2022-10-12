d149d91d2446bd950bc6b0551d598a4047cd6c83
Christian Hageloch <christian.hageloch@gmail.com>
2022-10-12 10:23:00 +0200

    Add a comment to remember what I did

--- lib/Custom/MyLayouts.hs
+++ lib/Custom/MyLayouts.hs
@@ -46,7 +46,10 @@ myLayoutHook = showWName' myShowWNameTheme
 
 -- I only use tiled and tabbed here because I  toggle
 -- fullscreen layout in MyKeys.hs
--- All the layouts that I use: tiled (Tall), tabbed, fullscreen (Full)
+-- Explanation for this line:
+-- the layout "tiled" is available on all workspaces, including workspace 1
+-- the layout "(noBorders tiled)" is available on every workspace except for workspace 1
+-- where it is replaced by the layout "(noBorders (tabbed shrinkText myTabConfig)"
 myLayout = tiled ||| onWorkspace "1:www" (noBorders (tabbed shrinkText myTabConfig)) (noBorders tiled)
   where
      -- default tiling algorithm partitions the screen into two panes
